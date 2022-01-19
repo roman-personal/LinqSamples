458d5c88925019f0e14e34543a89af30cee82136
Roman Tabunnikov <roman.tabunnikov@hotmail.com>
2022-01-19 20:47:06 +0300

    Palindrome

---
+++ LinqSamples/Palindrome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqSamples {
+    internal class Palindrome {
+        public static bool IsPalindrom(string text) {
+            if (string.IsNullOrEmpty(text))
+                return true;
+            var cleared = text
+                .Where(ch => char.IsLetterOrDigit(ch))
+                .Select(ch => char.ToUpper(ch));
+            return cleared.SequenceEqual(cleared.Reverse());
+        }
+    }
+}
--- LinqSamples/Program.cs
+++ LinqSamples/Program.cs
@@ -3,7 +3,11 @@
 namespace LinqSamples {
     internal class Program {
         static void Main(string[] args) {
-            // do nothing
+            // Palindrome
+            Console.WriteLine("ЭТО ПАЛИНДРОМ???");
+            string text = "А роза упала на лапу Азора";
+            Console.WriteLine(text);
+            Console.WriteLine(Palindrome.IsPalindrom(text));
         }
     }
 }
